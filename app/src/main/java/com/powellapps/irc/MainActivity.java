d9d7d2830dc3ae0c0d246eb2316897efb146ce79
Raphael <raphaelframos@gmail.com>
2020-01-11 00:58:34 -0300

    Recyclerview dos canais acessados criado junto com adapter

--- app/src/main/java/com/powellapps/irc/MainActivity.java
+++ app/src/main/java/com/powellapps/irc/MainActivity.java
@@ -1,13 +1,44 @@
 package com.powellapps.irc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import com.powellapps.irc.adapter.ChannelAdapter;
+import com.powellapps.irc.model.IrcChannel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ChannelAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerViewChannels = findViewById(R.id.reciclerChannels);
+        recyclerViewChannels.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewChannels.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        adapter = new ChannelAdapter();
+        recyclerViewChannels.setAdapter(adapter);
+
+        List<IrcChannel> channels = new ArrayList<>();
+        channels.add(new IrcChannel());
+        channels.add(new IrcChannel());
+        channels.add(new IrcChannel());
+        channels.add(new IrcChannel());
+        channels.add(new IrcChannel());
+        adapter.update(channels);
+
+
+
     }
 }
